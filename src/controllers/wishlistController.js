81010d3a158b880f46e64d0c9288db56b5e68576
MariaSernaD <lups.sernad@gmail.com>
2026-04-09 16:27:21 -0600

    adding all the endpoints for the wishlistController

--- src/controllers/wishlistController.js
+++ src/controllers/wishlistController.js
@@ -12,7 +12,6 @@ const getWishLists = async (req, res) => {
   }
 };
 
-
 const getUserWishlist = async (req, res) => {
   try {
     const { id } = req.params;
@@ -29,11 +28,10 @@ const getUserWishlist = async (req, res) => {
   }
 };
 
-
 const createWishlist = async (req, res) => {
   try {
     const { userId } = req.body;
-    // Si ya tiene wishlist, no crear otra
+    // Si ya existe una wishlist, no crear otra
     const wishlistExists = await Wishlist.findOne({ user: userId });
     if (wishlistExists) {
       return res.status(409).json({ message: "User already has a wishlist" });
@@ -47,16 +45,72 @@ const createWishlist = async (req, res) => {
   }
 };
 
+const addProductToWishlist = async (req, res) => {
+  try {
+    const { userId, productId } = req.body;
 
-const addProductToAWishlist = async (req, res) => {};
-const removeProductFromWishlist = async (req, res) => {};
-const deleteWishlist = async (req, res) => {};
+    const wishlist = await Wishlist.findOne({ user: userId });
+    if (!wishlist) {
+      return res.status(404).json({ message: "Wishlist not found" });
+    }
+    const alreadyAdded = wishlist.products.some(
+      (p) => p.product.toString() === productId,
+    );
+    if (alreadyAdded) {
+      return res.status(200).json({ message: "Product already in wishlist" });
+    }
+    wishlist.products.push({ product: productId });
+    await wishlist.save();
+    await wishlist.populate("user");
+    await wishlist.populate("products.product");
+
+    res.status(200).json(wishlist);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const removeProductFromWishlist = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { productId } = req.body;
+    const wishlist = await Wishlist.findById(id);
+    if (!wishlist) {
+      return res.status(404).json({ message: "Wishlist not found" });
+    }
+    //el filtro devuelve un nuevo array sin el producto que queremos eliminar
+    wishlist.products = wishlist.products.filter(
+      (p) => p.product.toString() !== productId,
+    );
+    await wishlist.save();
+    await wishlist.populate("user");
+    await wishlist.populate("products.product");
+    res.status(200).json(wishlist);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+const deleteWishlist = async (req, res) => {
+  try {
+    const {id}= req.params;
+    const wishlist = await Wishlist.findByIdAndDelete(id);
+    if(!wishlist){
+      return res.status(404).json({ message: "Wishlist not found" });
+    }
+    res.status(200).send("Wishlist is deleted");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 export {
   getWishLists,
   getUserWishlist,
   createWishlist,
-  addProductToAWishlist,
+  addProductToWishlist,
   removeProductFromWishlist,
   deleteWishlist,
 };
