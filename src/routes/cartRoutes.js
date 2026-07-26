d448eb9fbed0a8d0f113f0ffad295aa1e0080a3f
MariaSernaD <lups.sernad@gmail.com>
2026-07-25 18:51:23 -0600

    update: in the /cart route the isAdminMiddleware was added, to protected it. Also, the route of get cart by user was modified because security terms

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
+import isAdminMiddleware from "../middlewares/isAdminMiddleware.js"
 import {getCarts, getUserCart,addProductToCart , updateProductFromCart, deleteProductFromCart, clearCart} from "../controllers/cartController.js";
 
 const router = express.Router();
 
-router.get('/cart', authMiddleware, getCarts);
-router.get('/cart/user/:userId', authMiddleware, getUserCart);
+router.get('/cart', authMiddleware, isAdminMiddleware, getCarts);
+router.get('/cart/user/', authMiddleware, getUserCart);
 router.post('/cart/product', authMiddleware, addProductToCart);
 router.put('/cart/product', authMiddleware, updateProductFromCart);
 router.delete('/cart/product/:productId', authMiddleware, deleteProductFromCart);
